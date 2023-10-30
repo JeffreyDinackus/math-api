7ece4e888a2cf165fdc435a7697362486b0688c2
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-30 18:21:26 -0400

    adding error checking + documentation

--- app.py
+++ app.py
@@ -5,15 +5,23 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<equation>/<symbols>")
+@app.route("/<equation>/")
 def hello(equation=None, symbols=None):
-    answer = 1
-    symanswer = list(symbols)
+
+    if symbols != None and equation != None :
+
+        symanswer = list(symbols)
+        # problem = list(equation)
+    else: 
+        return { "request incomplete": "format: url/equation/symbolsUsed example: 127.0.0.1/2x-y/xy", "status_code" : 500}
     
+    # add in sympy processing, assign to answer.
+    
+    answer = 1
+
+
+
     return {"status_code" : 200,"input" : equation, "answer" : answer, "symbols" : symanswer}
- 
-# main driver function
+
 if __name__ == '__main__':
- 
-    # run() method of Flask class runs the application 
-    # on the local development server.
     app.run(debug=True)
