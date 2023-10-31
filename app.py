b3fb8fa4f2afc90a03fc9c47e970b31bd88c7b3a
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-31 11:21:12 -0400

    considering a hashmap, fixed json output to be uniform

--- app.py
+++ app.py
@@ -3,25 +3,32 @@ import sympy
 
 app = Flask(__name__)
 
+function_dict = {
+    "factor" : ""
+
+
+}
+
+
+
 @app.route("/")
 @app.route("/<function>/<equation>")
-@app.route("/<equation>/")
-def hello(equation=None, symbols=None):
+@app.route("/<function>")
+def hello(function=None, equation=None):
 
-    if symbols != None and equation != None :
-
-        symanswer = list(symbols)
-        # problem = list(equation)
+    if function != None and equation != None :
+        result = 1
+        #do processing
     else: 
         return { "request incomplete": "correct and resend", "format": "url/function/equation", "example" : "http://127.0.0.1:5000/factor/2x+1", "status_code" : 500}
     
     # add in sympy processing, assign to answer.
     
-    answer = 1
+    
 
 
 
-    return {"status_code" : 200,"input" : equation, "answer" : answer, "symbols" : symanswer}
+    return {"status_code" : 200,"input" : equation, "result" : result, "function" : function}
 
 if __name__ == '__main__':
     app.run(debug=True)
