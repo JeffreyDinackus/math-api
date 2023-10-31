75b95c5e91b89fe1f14fb160aab8b5dcc0432479
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-31 13:00:19 -0400

    adding some sympy factoring, may not work properly

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import sympy
+from sympy import *
 
 app = Flask(__name__)
 
@@ -17,10 +17,13 @@ function_dict = {
 def hello(function=None, equation=None):
 
     if function != None and equation != None :
-        result = 1
-        #do processing
+        
+        if function == "factor":
+            x, y, z = symbols('x y z')
+            result = cancel(eval(equation))
+        
     else: 
-        return { "request incomplete": "correct and resend", "format": "url/function/equation", "example" : "http://127.0.0.1:5000/factor/2x+1", "status_code" : 500}
+        return { "request incomplete": "correct and resend", "format": "url/function/equation", "example" : "http://127.0.0.1:5000/factor/x**2-2*x-8", "status_code" : 500}
     
     # add in sympy processing, assign to answer.
     
@@ -28,7 +31,7 @@ def hello(function=None, equation=None):
 
 
 
-    return {"status_code" : 200,"input" : equation, "result" : result, "function" : function}
+    return {"status_code" : 200,"input" : equation, "result" : str(result), "function" : function}
 
 if __name__ == '__main__':
     app.run(debug=True)
