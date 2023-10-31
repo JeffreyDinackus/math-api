b7ee7db82eef9a000122c19becb88f24ca6f103d
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-31 13:37:59 -0400

    adding data normalization

--- app.py
+++ app.py
@@ -16,16 +16,41 @@ a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = s
 @app.route("/<function>/<equation>")
 @app.route("/<function>")
 def hello(function=None, equation=None):
+    #data normalization
+    answer= ""
+    prev =""
+    for i in equation:
+
+        if i.isalpha() and prev.isdigit():
+            answer+="*"+i
+        elif i == "^":
+            answer+="**"
+        else:
+            answer+=i
+        prev = i
+   
 
 
     #implement hashmap for functions
     if function != None and equation != None :
         
         if function == "factor":
-            result = cancel(eval(equation))
+            result = factor(eval(answer))
+        
+
+    prev = ""
+    output = ""
+    for i in str(result):
+        if i == "*" and prev == "*":
+            output+="^"
+        if i.isalpha() and prev.isdigit():
+            
+
+
+        prev = i
         
     else: 
-        return { "request incomplete": "correct and resend", "format": "url/function/equation", "example" : "http://127.0.0.1:5000/factor/x**2-2*x-8", "status_code" : 500}
+        return { "request incomplete": "correct and resend", "format": "url/function/equation", "example" : "http://127.0.0.1:5000/factor/x^2-1", "status_code" : 500}
     
     # add in sympy processing, assign to answer.
     
