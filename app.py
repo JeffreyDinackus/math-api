49e7aaa67dcdc18752901dff5f1b66aba9b77700
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-31 14:15:26 -0400

    dict created

--- app.py
+++ app.py
@@ -4,10 +4,23 @@ from sympy import *
 app = Flask(__name__)
 
 function_dict = {
-    "factor" : ""
-
-
+    "1": "",
+    "2": "",
+    "3": "",
+    "4": "",
+    "5": "",
+    "6": "",
+    "7": "",
+    "8": "",
+    "9": "",
+    "10": "",
+    "11": "",
+    "12": "",
+    "13": "",
+    "14": "",
+    "15": ""
 }
+
 
 a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = symbols('a b c d e f g h i j k l m n o p q r s t u v w x y z')
 
